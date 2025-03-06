e812b61423168bbb36486d547d6f2171d929f18d
min <min.hf0948@gmail.com>
2025-03-07 00:15:19 +0900

    question 2

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.urls import path
+from django.template.response import TemplateResponse
 
 from .models import Choice, Question
+from . import views
 
 
 class ChoiceInline(admin.TabularInline):
@@ -10,18 +13,34 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
-    def change_view(self, request, object_id, form_url='', extra_context=None):
+    # 編集ページ修正
+    def change_view(self, request, object_id, form_url="", extra_context=None):
         extra = extra_context or {}
-        extra['title'] = 'questionを編集'
+        extra["title"] = "questionを編集"
 
-        return super(QuestionAdmin, self).change_view(request, object_id, form_url, extra_context=extra)
+        return super(QuestionAdmin, self).change_view(
+            request, object_id, form_url, extra_context=extra
+        )
+
+    # ページ追加
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                "new_page/",
+                self.admin_site.admin_view(views.NewPageView.as_view()),
+                name="new-page",
+            )
+        ]
+        return my_urls + urls
+
 
 admin.site.register(Question, QuestionAdmin)
