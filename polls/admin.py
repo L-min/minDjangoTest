608186baa11baae07f8a4f8940e5b08b4c1c5f8d
min <min.hf0948@gmail.com>
2025-03-07 00:15:19 +0900

    question 4, 5, 6

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.admin.utils import quote
+from django.contrib.admin.templatetags import admin_list
+from django.urls import path, reverse
 from django.utils import timezone
-
-# from django.template.response import TemplateResponse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from django.template.response import TemplateResponse
 
 from .models import Choice, Question
 from . import views
@@ -19,10 +22,16 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_display = [
+        "question_text",
+        "pub_date",
+        "was_published_recently",
+    ]
     list_filter = ["pub_date"]
+    list_display_links = ["pub_date"]
+    list_editable = ["question_text"]
+
     search_fields = ["question_text"]
-    # actions = ["change_question_text", "make_published"]
     actions = ["change_question_text"]
 
     # 編集ページ修正
@@ -42,7 +51,7 @@ class QuestionAdmin(admin.ModelAdmin):
                 "new_page/",
                 self.admin_site.admin_view(views.NewPageView.as_view()),
                 name="new-page",
-            )
+            ),
         ]
         return my_urls + urls
 
@@ -50,11 +59,54 @@ class QuestionAdmin(admin.ModelAdmin):
     def change_question_text(modeladmin, request, queryset):
         queryset.update(question_text="other")
 
-    # @admin.action(description="現在時刻で公開")
-    # def make_published(modeladmin, request, queryset):
-    #     now = timezone.now()
 
-    #     queryset.update(pub_date=now)
+class CustomQuestion(Question):
+    class Meta:
+        proxy = True
+
+
+class CustomQuestionAdmin(QuestionAdmin):
+    actions = ["change_question_text", "make_published"]
+    list_display = [
+        "question_text",
+        "detail_button",
+        "pub_date",
+        "was_published_recently",
+    ]
+    list_display_links = ["detail_button", "pub_date"]
+
+    @admin.action(description="現在時刻で公開")
+    def make_published(modeladmin, request, queryset):
+        now = timezone.now()
+
+        queryset.update(pub_date=now)
+
+    # カスタム一覧ページ
+    def changelist_view(self, request, extra_context=None):
+        app_label = self.opts.app_label
+
+        templateResponse = super().changelist_view(request, extra_context)
+        templateResponse.template_name = [
+            "admin/%s/%s/change_list_custom.html" % (app_label, self.opts.model_name),
+            "admin/%s/change_list_custom.html" % app_label,
+            "admin/change_list_custom.html",
+        ]
+        templateResponse.context = extra_context
+
+        return templateResponse
+
+    # 行に編集ボタン追加
+    def detail_button(self, obj):
+        buttonClasses = ["button", "default"]
+        row_class = mark_safe(' class="%s"' % " ".join(buttonClasses))
+
+        return format_html(
+            "<button {} type={} style='padding: 5px 10px;'>{}</button>",
+            row_class,
+            "button",
+            "編集する",
+        )
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(CustomQuestion, CustomQuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -7,22 +7,23 @@ from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
-        
-    # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+
     @admin.display(
         boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
+        ordering="pub_date",
+        description="Published recently?",
     )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Question2(Question):
+    pass
 
 
 class Choice(models.Model):
@@ -32,5 +33,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-    
